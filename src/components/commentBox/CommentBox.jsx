2a1d1a2135038417a25eaee73cd7a634b393b4c0
huzaifa <huzaifa98nur@gmail.com>
2022-03-01 14:36:44 +0500

    comment box finished

--- src/components/commentBox/CommentBox.jsx
+++ src/components/commentBox/CommentBox.jsx
@@ -4,8 +4,35 @@ import VoteButton from '../voteButton/VoteButton'
 export default function CommentBox() {
   let { comments } = data
   console.log(comments)
+  const commentReplies = (replies) => {
+    return (
+      <div className="container">
+        <div className="vote-button-container">
+          <VoteButton />
+          <div className="reply-desktop hide-on-desktop">
+            <ReplyButton />
+          </div>
+        </div>
+        <div className="comment-box">
+          <div className="comment-header">
+            <div className="left">
+              <img className="avatar-image" src={replies.user.image.webp} alt="" />
+              <div className="username">{replies.user.username}</div>
+              <div className="createdAt">{replies.createdAt}</div>
+            </div>
+            <div className="right hide-on-mobile">
+              <ReplyButton />
+            </div>
+          </div>
+          <div className="comment-body">
+            <p className="content">{replies.content}</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
 
-  const Reply = () => {
+  const ReplyButton = () => {
     return (
       <>
         <img src="/images/icon-reply.svg" alt="" />
@@ -20,7 +47,7 @@ export default function CommentBox() {
           <div className="vote-button-container">
             <VoteButton />
             <div className="reply-desktop hide-on-desktop">
-              <Reply />
+              <ReplyButton />
             </div>
           </div>
           <div className="comment-box">
@@ -31,13 +58,14 @@ export default function CommentBox() {
                 <div className="createdAt">{comment.createdAt}</div>
               </div>
               <div className="right hide-on-mobile">
-                <Reply />
+                <ReplyButton />
               </div>
             </div>
             <div className="comment-body">
               <p className="content">{comment.content}</p>
             </div>
           </div>
+          {comment.replies.length ? <commentReplies replies={comment.replies} /> : null}
         </div>
       ))}
     </>
